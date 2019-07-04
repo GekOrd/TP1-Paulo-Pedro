65f9a28958711093be0da62f31cc38d0adea52bb
PauloLimaTI <pauloiff2018@gmail.com>
2019-07-04 10:37:48 -0300

    Segunda parte concluida.

--- pichau/src/pichau/Cliente.java
+++ pichau/src/pichau/Cliente.java
@@ -10,5 +10,109 @@ package pichau;
  * @author aluno
  */
 public class Cliente {
-    
+    private String Name;
+    private String Last_name;
+    private int CPF;
+    private int RG;
+    private String email;
+    private String password;
+    private String confirm_password;
+
+    /**
+     * @return the Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    /**
+     * @param Name the Name to set
+     */
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    /**
+     * @return the Last_name
+     */
+    public String getLast_name() {
+        return Last_name;
+    }
+
+    /**
+     * @param Last_name the Last_name to set
+     */
+    public void setLast_name(String Last_name) {
+        this.Last_name = Last_name;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public int getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the RG
+     */
+    public int getRG() {
+        return RG;
+    }
+
+    /**
+     * @param RG the RG to set
+     */
+    public void setRG(int RG) {
+        this.RG = RG;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the confirm_password
+     */
+    public String getConfirm_password() {
+        return confirm_password;
+    }
+
+    /**
+     * @param confirm_password the confirm_password to set
+     */
+    public void setConfirm_password(String confirm_password) {
+        this.confirm_password = confirm_password;
+    }
 }
--- pichau/src/pichau/Endereco.java
+++ pichau/src/pichau/Endereco.java
@@ -10,18 +10,214 @@ package pichau;
  * @author aluno
  */
 public class Endereco {
-    int Telefone;
-    String CEP;
-    String Cidade;
-    String Estado;
-    String Rua;
-    String Bairro;
-    int Numero;
-    String complemento;
-    String Pais;
-    String Inscricao_Estadual;
-    int CPF_CNPJ;
-    int Celular;
-    String Empresa;
-    String Apelido;
+    private int Telefone;
+    private String CEP;
+    private String Cidade;
+    private String Estado;
+    private String Rua;
+    private String Bairro;
+    private int Numero;
+    private String complemento;
+    private String Pais;
+    private String Inscricao_Estadual;
+    private String CPF_CNPJ;
+    private String Celular;
+    private String Empresa;
+    private String Apelido;
+
+    /**
+     * @return the Telefone
+     */
+    public int getTelefone() {
+        return Telefone;
+    }
+
+    /**
+     * @param Telefone the Telefone to set
+     */
+    public void setTelefone(int Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    /**
+     * @return the CEP
+     */
+    public String getCEP() {
+        return CEP;
+    }
+
+    /**
+     * @param CEP the CEP to set
+     */
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    /**
+     * @return the Cidade
+     */
+    public String getCidade() {
+        return Cidade;
+    }
+
+    /**
+     * @param Cidade the Cidade to set
+     */
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    /**
+     * @return the Estado
+     */
+    public String getEstado() {
+        return Estado;
+    }
+
+    /**
+     * @param Estado the Estado to set
+     */
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    /**
+     * @return the Rua
+     */
+    public String getRua() {
+        return Rua;
+    }
+
+    /**
+     * @param Rua the Rua to set
+     */
+    public void setRua(String Rua) {
+        this.Rua = Rua;
+    }
+
+    /**
+     * @return the Bairro
+     */
+    public String getBairro() {
+        return Bairro;
+    }
+
+    /**
+     * @param Bairro the Bairro to set
+     */
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    /**
+     * @return the Numero
+     */
+    public int getNumero() {
+        return Numero;
+    }
+
+    /**
+     * @param Numero the Numero to set
+     */
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the Pais
+     */
+    public String getPais() {
+        return Pais;
+    }
+
+    /**
+     * @param Pais the Pais to set
+     */
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+
+    /**
+     * @return the Inscricao_Estadual
+     */
+    public String getInscricao_Estadual() {
+        return Inscricao_Estadual;
+    }
+
+    /**
+     * @param Inscricao_Estadual the Inscricao_Estadual to set
+     */
+    public void setInscricao_Estadual(String Inscricao_Estadual) {
+        this.Inscricao_Estadual = Inscricao_Estadual;
+    }
+
+    /**
+     * @return the CPF_CNPJ
+     */
+    public String getCPF_CNPJ() {
+        return CPF_CNPJ;
+    }
+
+    /**
+     * @param CPF_CNPJ the CPF_CNPJ to set
+     */
+    public void setCPF_CNPJ(String CPF_CNPJ) {
+        this.CPF_CNPJ = CPF_CNPJ;
+    }
+
+    /**
+     * @return the Celular
+     */
+    public String getCelular() {
+        return Celular;
+    }
+
+    /**
+     * @param Celular the Celular to set
+     */
+    public void setCelular(String Celular) {
+        this.Celular = Celular;
+    }
+
+    /**
+     * @return the Empresa
+     */
+    public String getEmpresa() {
+        return Empresa;
+    }
+
+    /**
+     * @param Empresa the Empresa to set
+     */
+    public void setEmpresa(String Empresa) {
+        this.Empresa = Empresa;
+    }
+
+    /**
+     * @return the Apelido
+     */
+    public String getApelido() {
+        return Apelido;
+    }
+
+    /**
+     * @param Apelido the Apelido to set
+     */
+    public void setApelido(String Apelido) {
+        this.Apelido = Apelido;
+    }
 }
