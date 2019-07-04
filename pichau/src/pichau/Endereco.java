269d4a65d10832bcd01663292484b2faf68567a0
GekOrdep <pedrovrcamargo@gmail.com>
2019-07-04 10:05:28 -0300

    terceira parte concluida.

---
+++ pichau/src/pichau/Endereco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pichau;
+
+/**
+ *
+ * @author aluno
+ */
+public class Endereco {
+    int Telefone;
+    String CEP;
+    String Cidade;
+    String Estado;
+    String Rua;
+    String Bairro;
+    int Numero;
+    String complemento;
+    String Pais;
+    String Inscricao_Estadual;
+    int CPF_CNPJ;
+    int Celular;
+    String Empresa;
+    String Apelido;
+}
